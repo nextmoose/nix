110c1691e7cef902bb78cb822fffb66c590e199c
Emory Merryman <emory.merryman@gmail.com>
2020-07-05 20:33:47 +0000

    fix timers

--- public/scripts/structure-timers/structure-timers.sh
+++ public/scripts/structure-timers/structure-timers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo date --date $(date --date $((${@})) +%Y-%m-%d-%H:%M) +%s &&
-    echo date --date $(date --date @$((${@}+60*5)) +%s) +%s &&
-    echo date --date @$(date --date $((${@}+60)) +%Y-%m-%d-%H:%M) +%s &&
-    echo date --date $(date --date @$((${@}+60*6)) +%s) +%s &&
+echo date --date $(date --date @$((${@})) +%Y-%m-%d-%H:%M) +%s &&
+    echo date --date @$(date --date @$((${@}+60*5)) +%s) +%s &&
+    echo date --date $(date --date @$((${@}+60)) +%Y-%m-%d-%H:%M) +%s &&
+    echo date --date @$(date --date @$((${@}+60*6)) +%s) +%s &&
     true
