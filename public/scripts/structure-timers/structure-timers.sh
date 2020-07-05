c6805a640a8ca3fb16ba5f97181305de646cd407
Emory Merryman <emory.merryman@gmail.com>
2020-07-05 21:12:01 +0000

    fix timers

--- public/scripts/structure-timers/structure-timers.sh
+++ public/scripts/structure-timers/structure-timers.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-date --date "$(date --date @$((${@})) \"+%Y-%m-%d %H:%M\")" +%s &&
-    date --date @$(date --date @$((${@}+60*5)) +%s) +%s &&
-    date --date "$(date --date @$((${@}+60)) \"+%Y-%m-%d %H:%M\")" +%s &&
-    date --date @$(date --date @$((${@}+60*6)) +%s) +%s &&
+ALPHA=$(date --date @$((${@})) "+%Y-%m-%d %H:%M") &&
+    BETA=$(date --date @$((${@}+60*5)) +%s) &&
+    GAMMA=$(date --date @$((${@}+60)) "+%Y-%m-%d %H:%M") &&
+    DELTA=$(date --date @$((${@}+60*6)) +%s) &&
+    date --date "${ALPHA}" +%s &&
+    date --date @${BETA} +%s &&
+    date --date "${GAMMA}" +%s &&
+    date --date @${DELTA} +%s &&
     true
