d1627cdc2b52252a2d2dbb0f7e572ced62abbb03
Emory Merryman <emory.merryman@gmail.com>
2020-07-05 20:19:55 +0000

    fix timers

--- public/scripts/structure/structure.sh
+++ public/scripts/structure/structure.sh
@@ -7,24 +7,28 @@ NOW=$( date +%s ) &&
 	    true
     fi &&
     fun() {
-	read GROUPER &&
-	    read CLEANER &&
-	    HASH=$( echo "${CONSTRUCTOR} ${SALT} ${GROUPER} ${DESTRUCTOR}" | md5sum | cut 1-32  ) &&
+	read GROUP_TIMESTAMP &&
+	    read CLEAN_TIMESTAMP &&
+	    SALT=$(${SALT_PROGRAM}) &&
+	    HASH=$( echo "${CONSTRUCTOR_PROGRAM} ${SALT} ${GROUP_TIMESTAMP} ${CLEANER_PROGRAM}" | md5sum | cut 1-32  ) &&
 	    echo "NOW=${NOW}" >> "${STRUCTURES_DIR}/${HASH}.log" &&
 	    if [ -d "${STRUCTURES_DIR}/${HASH}" ]
 	    then
-		echo "${STRUCTURES_DIR}/${HASH}" &&
+		( flock --shared 203 || exit 1
+		  echo "${STRUCTURES_DIR}/${HASH}" &&
+		      true
+		) 203> "${STRUCTURES_DIR}/${HASH}.lock" &&
 		    true
 	    else
-		mkdir "${STRUCTURES_DIR}/${HASH}" &&
-		    cd "${STRUCTURES_DIR}/${HASH}" &&
-		    BEFORE=$(date +%s) &&
-		    ( "${CONSTRUCTOR}" > "${STRUCTURES_DIR}/${HASH}.out" 2> "${STRUCTURES_DIR}/${HASH}.err" > "${STRUCTURES_DIR}/${HASH}.time" || true ) &&
-		    EXIT_CODE="${?}" &&
-		    AFTER=$(date +%s) &&
-		    (cat > "${STRUCTURES_DIR}/${HASH}.log" <<EOF
-NOW=${NOW}
-CONSTRUCTOR=${CONSTRUCTOR}
+		( flock 201 || exit 1 
+		  mkdir "${STRUCTURES_DIR}/${HASH}" &&
+		      cd "${STRUCTURES_DIR}/${HASH}" &&
+		      BEFORE=$(date +%s) &&
+		      ( "${CONSTRUCTOR}" > "${STRUCTURES_DIR}/${HASH}.out" 2> "${STRUCTURES_DIR}/${HASH}.err" > "${STRUCTURES_DIR}/${HASH}.time" || true ) &&
+		      EXIT_CODE="${?}" &&
+		      AFTER=$(date +%s) &&
+		      (cat > "${STRUCTURES_DIR}/${HASH}.log" <<EOF
+CONSTRUCTOR_PROGRAM=${CONSTRUCTOR}
 SALT=${SALT}
 TIMERS=${TIMERS}
 DESTRUCTOR=${DESTRUCTOR}
@@ -33,15 +37,16 @@ CLEANER=${CLEANER}
 EXIT_CODE=${EXIT_CODE}
 CONSTRUCTION_TIME=$((${AFTER}-${BEFORE}))
 EOF
-		    ) &&
-		    if [ "${EXIT_CODE}" != 0 ]
-		    then
-			mkdir "${STRUCTURES_DIR}/${HASH}.${NOW}" &&
-			    mv "${STRUCTURES_DIR}/${HASH}" "${STRUCTURES_DIR}/${HASH}.log" "${STRUCTURES_DIR}/${HASH}.out" "${STRUCTURES_+DIR}/${HASH}.err" "${STRUCTURES_DIR}/${HASH}.${NOW}" &&
-			    true
-		    fi &&
-		    at 
-		    true
+		      ) &&
+		      if [ "${EXIT_CODE}" == 0 ]
+		      then
+			  echo "cd ${STRUCTURES_DIR}/${HASH} && ${CLEAN} && cd / && rm --recursive --force ${STRUCTURES_DIR}/${HASH} ${STRUCTURES_DIR}/${HASH}.log ${STRUCTURES_DIR}/${HASH}.out ${STRUCTURES_DIR}/${HASH}.err ${STRUCTURES_DIR}/${HASH}." | at $(date --date ${CLEANER
+		      else
+			  mkdir "${STRUCTURES_DIR}/${HASH}.${NOW}" &&
+			      mv "${STRUCTURES_DIR}/${HASH}" "${STRUCTURES_DIR}/${HASH}.log" "${STRUCTURES_DIR}/${HASH}.out" "${STRUCTURES_+DIR}/${HASH}.err" "${STRUCTURES_DIR}/${HASH}.${NOW}" &&
+			      true
+		      fi &&
+		  true
 	    fi &&
 	    true
     } &&
