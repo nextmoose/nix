2d38c7e8af039f6e6f00ea7092d71017c44e5e9e
Emory Merryman <emory.merryman@gmail.com>
2020-07-07 16:09:33 +0000

    work on structure

--- public/scripts/structure/structure.sh
+++ public/scripts/structure/structure.sh
@@ -1,70 +1,74 @@
 #!/bin/sh
 
-NOW=$(date +%s) &&
+NOW=$( date +%s ) &&
     if [ ! -d "${STRUCTURES_DIR}" ]
     then
 	mkdir "${STRUCTURES_DIR}" &&
 	    true
     fi &&
-    fun() {
-	read GROUP_TIMESTAMP &&
-	    read DESTRUCTION_TIMESTAMP &&
-	    SALT=$( ${SALT_PROGRAM} ) &&
-	    HASH=$(echo "${CONSTRUCTOR_PROGRAM} ${GROUP_TIMESTAMP} ${SALT} ${CLEANER_PROGRAM}" | md5sum | cut --bytes 1-32) &&
-	    if [ ! -d "${STRUCTURES_DIR}/${HASH}.debug" ]
-	    then
-		mkdir "${STRUCTURES_DIR}/${HASH}.debug" &&
-		    true
-	    fi &&
-	    (
-		flock --shared 201 || exit 1
-		if [ -d "${STRUCTURES_DIR}/${HASH}" ]
-		then
-		    echo "NOW=${NOW}" >> "${STRUCTURES_DIR}/${HASH}.log" &&
-			echo "${STRUCTURES_DIR}/${HASH}" &&
-			true
-		else
-		    (
-			flock --shared 202 || exit 1
-			WORK_DIR=$( mktemp -d "${STRUCTURES_DIR}/${HASH}.debug/XXXXXXXX" ) &&
-			    mkdir "${WORK_DIR}/${HASH}" &&
-			    cd "${WORK_DIR}/${HASH}" &&
-			    BEFORE=$(date +%s) &&
-			    ( "${CONSTRUCTOR_PROGRAM}" > "${WORK_DIR}/${HASH}.log" 2> "${WORK_DIR}/${HASH}.err" || true ) &&
-			    EXIT_CODE="${?}" &&
-			    AFTER=$(date +%s) &&
-			    (cat > "${WORK_DIR}/${HASH}.log" <<EOF
-CONSTRUCTOR_PROGRAM=${CONSTRUCTOR_PROGRAM}
-NOW=${NOW}
-TIMERS_PROGRAM=${TIMERS_PROGRAM}
-GROUP_TIMESTAMP=${GROUP_TIMESTAMP}
-DESTRUCTION_TIMESTAMP=${DESTRUCTION_TIMESTAMP}
-SALT_PROGRAM=${SALT_PROGRAM}
-SALT=${SALT}
-CLEANING_PROGRAM=${CLEANING_PROGRAM}
-EXECUTION_TIME=$((${AFTER}-${BEFORE}))
+    SALT=$( ${SALT_PROGRAM} ) &&
+    SCHEDULED_DESTRUCTION_TIME=$( date --date @$((((${NOW}+${SECONDS})/${SECONDS})*${SECONDS})) --date "%H:%M %Y-%m-%d" ) &&
+    HASH=$( ( cat <<EOF
+${CONSTRUCTOR_PROGRAM}
+${CLEANER_PROGRAM}
+${SALT}
+${SCHEDULED_DESTRUCTION_TIME}
 EOF
-			    ) &&
-			    if [ "${EXIT_CODE}" == 0 ]
-			    then
-				D=$(date --date @${DESTRUCTION_TIMESTAMP} +"%H:%M %Y-%m-%d") &&
-				    which at &&
-				    echo "${DESTRUCTOR_PROGRAM} ${CLEANING_PROGRAM} ${HASH}" | at "${D}" &&
-				    mv "${WORK_DIR}/${HASH}" "${WORK_DIR}/${HASH}.log" "${WORK_DIR}/${HASH}.out" "${WORK_DIR}/${HASH}.err" "${STRUCTURES_DIR}" &&
-				    rm --recursive --force "${WORK_DIR}" &&
-				    echo "${STRUCTURES_DIR}/${HASH}" &&
-				    true
-			    else
-				"${CLEANING_PROGRAM}" &&
-				    echo "${WORK_DIR}/${HASH}" &&
-				    true
-			    fi &&
-			    true
-		    ) 202> "${STRUCTURES_DIR}/${HASH}.lock" &&
-			    true
+	    ) | md5sum | cut --bytes 1-32 ) &&
+    (
+	flock --shared 202 || exit 1
+	if [ -d "${STRUCTURES_DIR}/${HASH}" ]
+	then
+	    echo "NOW=${NOW}" >> "${STRUCTURES_DIR}/${HASH}.log" &&
+		echo "${STRUCTURES_DIR}/${HASH}" &&
+		true
+	else
+	    (
+		flock 203 || exit 1
+		if [ -d "${STRUCTURES_DIR}/${HASH}.debug" ]
+		then
+		    DEBUG_DIR=$( mktemp -d "${STRUCTURES_DIR}/${HASH}.debug/XXXXXXXX" ) &&
+			cd "${DEBUG_DIR}" &&
+			BEFORE=$( date +%s ) &&
+			"${CONSTRUCTOR_PROGRAM}" > "${DEBUG_DIR}/${HASH}.out" 2> "${DEBUG_DIR}/${HASH}.err" &&
+			EXIT_CODE="${?}" &&
+			AFTER=$( date +%s ) &&
+			( cat > "${DEBUG_DIR}/${HASH}.log" <<EOF
+NOW=${NOW}
+CONSTRUCTOR_PROGRAM=${CONSTRUCTOR_PROGRAM}
+CLEANER_PROGRAM=${CLEANER_PROGRAM}
+SALT=${SALT}
+SCHEDULED_DESTRUCTION_TIME=${SCHEDULED_DESTRUCTION_TIME}
+EXIT_CODE=${EXIT_CODE}
+CONSTRUCTION_DURATION=$((${AFTER}-${BEFORE}))
+EOF
+			) &&
+			if [ "${EXIT_CODE}" == 0 ]
+			then
+			    mv "${DEBUG_DIR}/${HASH}" "${DEBUG_DIR}/${HASH}.log" "${DEBUG_DIR}/${HASH}.out" "${DEBUG_DIR}/${HASH}.err" "${STRUCTURES_DIR}" &&
+				( cat <<EOF
+(
+flock --exclusive 204 || exit 1
+cd "${STRUCTURES_DIR}/${HASH}" &&
+"${CLEANER_PROGRAM}"  &&
+cd / &&
+rm --recursive --force "${STRUCTURES_DIR}/${HASH}" "${STRUCTURES_DIR}/${HASH}.log" "${STRUCTURES_DIR}/${HASH}.out" "${STRUCTURES_DIR}/${HASH}.err" "${STRUCTURES_DIR}/${HASH}.debug"
+) 204> ${STRUCTURES_DIR}/${HASH}.lock 
+EOF
+				) | at "${SCHEDULED_DESTRUCTION_TIME" &&
+				echo "${STRUCTURES_DIR}/${HASH}" &&
+				true
+			else
+			    "${CLEANER_PROGRAM}" &&
+				echo "${DEBUG_DIR}/${HASH}" &&
+				true
+			fi &&
+			true
 		fi &&
 		    true
-	    ) 201> "${STRUCTURES_DIR}/${HASH}.lock" &&
+	    ) > "${STRUCTURES_DIR}/lock" &&
+		true
+	fi &&
 	    true
-    } &&
-    "${TIMERS_PROGRAM}" "${NOW}" | fun
+    ) > "${STRUCTURES_DIR}/${HASH}.lock" &&
+    true
