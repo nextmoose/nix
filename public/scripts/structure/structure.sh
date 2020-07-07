670488f20a588058add9187d5ba7d98f027af2db
Emory Merryman <emory.merryman@gmail.com>
2020-07-07 18:44:41 +0000

    work on structure

--- public/scripts/structure/structure.sh
+++ public/scripts/structure/structure.sh
@@ -27,13 +27,16 @@ EOF
 		flock 203 || exit 1
 		if [ -d "${STRUCTURES_DIR}/${HASH}.debug" ]
 		then
+		    mkdir "${STRUCTURES_DIR}/${HASH}.debug" &&
+			true
+		fi &&
 		    DEBUG_DIR=$( mktemp -d "${STRUCTURES_DIR}/${HASH}.debug/XXXXXXXX" ) &&
-			cd "${DEBUG_DIR}" &&
-			BEFORE=$( date +%s ) &&
-			"${CONSTRUCTOR_PROGRAM}" > "${DEBUG_DIR}/${HASH}.out" 2> "${DEBUG_DIR}/${HASH}.err" &&
-			EXIT_CODE="${?}" &&
-			AFTER=$( date +%s ) &&
-			( cat > "${DEBUG_DIR}/${HASH}.log" <<EOF
+		    cd "${DEBUG_DIR}" &&
+		    BEFORE=$( date +%s ) &&
+		    ( "${CONSTRUCTOR_PROGRAM}" > "${DEBUG_DIR}/${HASH}.out" 2> "${DEBUG_DIR}/${HASH}.err" || true ) &&
+		    EXIT_CODE="${?}" &&
+		    AFTER=$( date +%s ) &&
+		    ( cat > "${DEBUG_DIR}/${HASH}.log" <<EOF
 NOW=${NOW}
 CONSTRUCTOR_PROGRAM=${CONSTRUCTOR_PROGRAM}
 CLEANER_PROGRAM=${CLEANER_PROGRAM}
@@ -42,11 +45,11 @@ SCHEDULED_DESTRUCTION_TIME=${SCHEDULED_DESTRUCTION_TIME}
 EXIT_CODE=${EXIT_CODE}
 CONSTRUCTION_DURATION=$((${AFTER}-${BEFORE}))
 EOF
-			) &&
-			if [ "${EXIT_CODE}" == 0 ]
-			then
-			    mv "${DEBUG_DIR}/${HASH}" "${DEBUG_DIR}/${HASH}.log" "${DEBUG_DIR}/${HASH}.out" "${DEBUG_DIR}/${HASH}.err" "${STRUCTURES_DIR}" &&
-				( cat <<EOF
+		    ) &&
+		    if [ "${EXIT_CODE}" == 0 ]
+		    then
+			mv "${DEBUG_DIR}/${HASH}" "${DEBUG_DIR}/${HASH}.log" "${DEBUG_DIR}/${HASH}.out" "${DEBUG_DIR}/${HASH}.err" "${STRUCTURES_DIR}" &&
+			    ( cat <<EOF
 (
 flock --exclusive 204 || exit 1
 cd "${STRUCTURES_DIR}/${HASH}" &&
@@ -55,16 +58,14 @@ cd / &&
 rm --recursive --force "${STRUCTURES_DIR}/${HASH}" "${STRUCTURES_DIR}/${HASH}.log" "${STRUCTURES_DIR}/${HASH}.out" "${STRUCTURES_DIR}/${HASH}.err" "${STRUCTURES_DIR}/${HASH}.debug"
 ) 204> ${STRUCTURES_DIR}/${HASH}.lock 
 EOF
-				) | at "${SCHEDULED_DESTRUCTION_TIME}" &&
-				echo "${STRUCTURES_DIR}/${HASH}" &&
-				true
-			else
-			    "${CLEANER_PROGRAM}" &&
-				echo "${DEBUG_DIR}/${HASH}" &&
-				true
-			fi &&
-			true
-		fi &&
+			    ) | at "${SCHEDULED_DESTRUCTION_TIME}" &&
+			    echo "${STRUCTURES_DIR}/${HASH}" &&
+			    true
+		    else
+			"${CLEANER_PROGRAM}" &&
+			    echo "${DEBUG_DIR}/${HASH}" &&
+			    true
+		    fi &&
 		    true
 	    ) 203> "${STRUCTURES_DIR}/${HASH}.lock" &&
 		true
