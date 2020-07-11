c8b08282d07a7c8e32418ca02124727607170889
Emory Merryman <emory.merryman@gmail.com>
2020-07-11 20:06:58 +0000

    get rid of locks after done

--- public/scripts/destructor/destructor.sh
+++ public/scripts/destructor/destructor.sh
@@ -13,7 +13,9 @@ CLEANER_PROGRAM="${1}" &&
 		    rm --recursive --force "${STRUCTURES_DIR}/${HASH}" "${STRUCTURES_DIR}/${HASH}.log" "${STRUCTURES_DIR}/${HASH}.out" "${STRUCTURES_DIR}/${HASH}.err" "${STRUCTURES_DIR}/${HASH}.debug" "${STRUCTURES_DIR}/${HASH}.at" &&
 		    true
 	    ) 202> "${STRUCTURES_DIR}/${HASH}.exclusive" &&
+	    rm "${STRUCTURES_DIR}/${HASH}.exclusive" &&
 	    true
     ) 201> "${STRUCTURES_DIR}/${HASH}.shared" &&
+    rm "${STRUCTURES_DIR}/${HASH}.shared" &&
     true
 
