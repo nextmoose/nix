8a087afc326e6d1ac5bc238fd01a173db50c3e75
Emory Merryman <emory.merryman@gmail.com>
2020-07-11 19:56:29 +0000

    fix error with exclusive lock

--- public/scripts/destructor/destructor.sh
+++ public/scripts/destructor/destructor.sh
@@ -12,7 +12,7 @@ CLEANER_PROGRAM="${1}" &&
 		    cd "${STRUCTURES_DIR}" &&
 		    rm --recursive --force "${STRUCTURES_DIR}/${HASH}" "${STRUCTURES_DIR}/${HASH}.log" "${STRUCTURES_DIR}/${HASH}.out" "${STRUCTURES_DIR}/${HASH}.err" "${STRUCTURES_DIR}/${HASH}.debug" "${STRUCTURES_DIR}/${HASH}.at" &&
 		    true
-	    ) 202 > "${STRUCTURES_DIR}/${HASH}.exclusive" &&
+	    ) 202> "${STRUCTURES_DIR}/${HASH}.exclusive" &&
 	    true
     ) 201> "${STRUCTURES_DIR}/${HASH}.shared" &&
     true
