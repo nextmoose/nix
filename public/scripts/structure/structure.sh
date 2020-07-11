06b4cb7870767772a0dda71417288831cf29309d
Emory Merryman <emory.merryman@gmail.com>
2020-07-11 02:38:18 +0000

    redo structure

--- public/scripts/structure/structure.sh
+++ public/scripts/structure/structure.sh
@@ -1,28 +1,22 @@
 #!/bin/sh
 
-(
-    (
-	flock 301 || exit true
-    ) &&
-	NOW=$( date +%s ) &&
-	if [ ! -d "${STRUCTURES_DIR}" ]
-	then
-	    mkdir "${STRUCTURES_DIR}" &&
-		true
-	fi &&
-	SALT=$( ${SALT_PROGRAM} ) &&
-	SCHEDULED_DESTRUCTION_TIME=$( date --date @$((((${NOW}+${SECONDS})/${SECONDS})*${SECONDS})) "+%H:%M %Y-%m-%d" ) &&
-	HASH=$( ( cat <<EOF
+NOW=$( date +%s ) &&
+    if [ ! -d "${STRUCTURES_DIR}" ]
+    then
+	mkdir "${STRUCTURES_DIR}" &&
+	    true
+    fi &&
+    SALT="$( ${SALT_PROGRAM} )" &&
+    SCHEDULED_DESTRUCTION_TIME=$( date --date $(( ( ( $( date +%s ) + ${SECONDS} ) / ${SECONDS} ) * ${SECONDS} )) +%s ) &&
+    HASH=( ( cat <<EOF
 ${CONSTRUCTOR_PROGRAM}
 ${CLEANER_PROGRAM}
 ${SALT}
 ${SCHEDULED_DESTRUCTION_TIME}
 EOF
-		) | md5sum | cut --bytes 1-32 ) &&
-	(
-	    (
-		flock --shared 303 || exit 1
-	    ) &&
+	   ) | md5sum | cut --bytes 1-32 ) &&
+    (
+	( flock --shared 201 || exit 1 ) &&
 	    if [ -d "${STRUCTURES_DIR}/${HASH}" ]
 	    then
 		echo "NOW=${NOW}" >> "${STRUCTURES_DIR}/${HASH}.log" &&
@@ -30,55 +24,48 @@ EOF
 		    true
 	    else
 		(
-		    ( flock --shared 304 || exit 1 ) &&
+		    ( flock 202 || exit 1 ) &&
 			if [ ! -d "${STRUCTURES_DIR}/${HASH}.debug" ]
 			then
 			    mkdir "${STRUCTURES_DIR}/${HASH}.debug" &&
 				true
 			fi &&
 			DEBUG_DIR=$( mktemp -d "${STRUCTURES_DIR}/${HASH}.debug/XXXXXXXX" ) &&
-			mkdir "${DEBUG_DIR}/${HASH}" &&
-			cd "${DEBUG_DIR}/${HASH}" &&
+			cd "${DEBUG_DIR}" &&
 			BEFORE=$( date +%s ) &&
-			( "${CONSTRUCTOR_PROGRAM}" > "${DEBUG_DIR}/${HASH}.out" 2> "${DEBUG_DIR}/${HASH}.err" || true ) &&
+			"${CONSTRUCTOR}" > "${DEBUG_DIR}/${HASH}.out" 2> "${DEBUG_DIR}/${HASH}.err" &&
 			EXIT_CODE="${?}" &&
 			AFTER=$( date +%s ) &&
 			( cat > "${DEBUG_DIR}/${HASH}.log" <<EOF
-NOW=${NOW}
-CONSTRUCTOR_PROGRAM=${CONSTRUCTOR_PROGRAM}
+CONSTRUCTOR_PROGRAM= ${CONSTRUCTOR_PROGRAM}
 CLEANER_PROGRAM=${CLEANER_PROGRAM}
 SALT=${SALT}
-SCHEDULED_DESTRUCTION_TIME=${SCHEDULED_DESTRUCTION_TIME}
-EXIT_CODE=${EXIT_CODE}
-CONSTRUCTION_DURATION=$((${AFTER}-${BEFORE}))
-HASH=${HASH}
+SCHEDULE_DESTRUCTION_TIME=${SCHEDULED_DESTRUCTION_TIME}
+ELAPSED_CONSTRUCTION_TIME=$(( ${AFTER} -  ${BEFORE} ))
 EOF
 			) &&
 			if [ "${EXIT_CODE}" == 0 ]
 			then
-			    mv "${DEBUG_DIR}/${HASH}" "${DEBUG_DIR}/${HASH}.log" "${DEBUG_DIR}/${HASH}.out" "${DEBUG_DIR}/${HASH}.err" "${STRUCTURES_DIR}" &&
-				rm --recursive --force "${DEBUG_DIR}" &&
+			    cd "${STRUCTURES_DIR" &&
+			        mv "${DEBUG_DIR}/${HASH}" "${DEBUG_DIR}/${HASH}.log" "${DEBUG_DIR}/${HASH}.out" "${DEBUG_DIR}/${HASH}.err" "${STRUCTURES_DIR}" &&
 				( cat <<EOF
 (
-    ( flock --shared 301 || exit 1 ) &&
+    ( flock 201 || exit 1 ) &&
         (
-            ( flock --shared 303 || exit 1 ) &&
-                (
-                    ( flock 302 || exit 1 ) &&
-                        cd "${STRUCTURES_DIR}/${HASH}" &&
-                        "${CLEANER_PROGRAM}" > "${STRUCTURES_DIR}/${HASH}.clean" 2>&1 &&
-                        rm --recursive --force "${STRUCTURES_DIR}/${HASH}" "${STRUCTURES_DIR}/${HASH}.log" "${STRUCTURES_DIR}/${HASH}.out" "${STRUCTURES_DIR}/${HASH}.err" "${STRUCTURES_DIR}/${HASH}.at" "${STRUCTURES_DIR}/${HASH}.debug" "${STRUCTURES_DIR}/${HASH}.clean" &&
-                        true
-                ) 302> "${STRUCTURES_DIR}/${HASH}.exlusive" &&
-		rm "${STRUCTURES_DIR}/${HASH}.exclusive" &&
+            ( flock --exclusive 202 || exit 1 ) &&
+                cd "${STRUCTURES_DIR}/${HASH}" &&
+		"${CLEANER_PROGRAM}" &&
+		cd "${STRUCTURES_DIR}" &&
+		rm --recursive --force "${STRUCTURES_DIR}/${HASH}" "${STRUCTURES_DIR}/${HASH}.log" "${STRUCTURES_DIR}/${HASH}.out" "${STRUCTURES_DIR}/${HASH}.err" "${STRUCTURES_DIR}/${HASH}.debug" "${STRUCTURES_DIR}/${HASH}.at" &&
                 true
-        ) 303> "${STRUCTURES_DIR}/${HASH}.shared" &&
-	rm "${STRUCTURES_DIR}/${HASH}.shared" &&
-        true
-) 301> "${STRUCTURES_DIR}.shared" &&
+        ) 202> "${STRUCTURES_DIR}/${HASH}.exclusive &&
+	    rm "${STRUCTURES_DIR}/${HASH}.exclusive &&
+            true
+) 201> "${STRUCTURES_DIR}/${HASH}.shared &&
+    rm "${STRUCTURES_DIR}/${HASH}.shared &&
     true
 EOF
-				) | /run/wrappers/bin/at "${SCHEDULED_DESTRUCTION_TIME}" > "${STRUCTURES_DIR}/${HASH}.at" 2>&1 && ## KLUDGE
+				) | at $( date --date "${SCHEDULED_DESTRUCTION_TIME}" "+%Y-%m-%d %H:%M" ) &&
 				echo "${STRUCTURES_DIR}/${HASH}" &&
 				true
 			else
@@ -87,11 +74,9 @@ EOF
 				true
 			fi &&
 			true
-		) 304> "${STRUCTURES_DIR}/${HASH}.exclusive" &&
+		) > "${STRUCTURES_DIR}/${HASH}.exclusive" &&
 		    true
 	    fi &&
-		true
-	) 303> "${STRUCTURES_DIR}/${HASH}.shared" &&
-	true
-) 301> "${STRUCTURES_DIR}.shared" &&
+	    true
+    ) 201> "${STRUCTURES_DIR}/${HASH}.shared" &&
     true
