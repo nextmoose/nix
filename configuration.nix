540b47aa9096a921cebc865ebb247b01d857ae1a
Emory Merryman <emory.merryman@gmail.com>
2020-07-11 01:51:00 +0000

    literal

--- configuration.nix
+++ configuration.nix
@@ -1,21 +1,8 @@
 { config, pkgs, ... } : let
-    kludge = {
-        at = pkgs.stdenv.mkDerivation {
-	    name = "at" ;
-	    src = ./. ;
-	    buildInputs = [ pkgs.makeWrapper ] ;
-	    installPhase = "makeWrapper /run/wrappers/bin/at $out/bin/at" ;
-	} ;
-    } ;
+    literal = value : name : "--run \"export ${ utils.upper-case name }=${ builtins.replaceStrings [ "\"" ] [ "\\\"" ] ( builtins.toString value ) }\"" ;
     utils = {
         name-it = named : builtins.listToAttrs ( builtins.map ( name : { name = name ; value = builtins.getAttr name named name ; } ) ( builtins.attrNames named ) ) ;
-        sh-derivation = name : sets : dependencies : let
-	    upper-case = string : builtins.replaceStrings [ "-" "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" ] [ "_" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" ] string ;
-	    s = sets {
-	        literal = name : value : "--run \"export ${ upper-case name}=${ builtins.replaceStrings [ "\"" ] [ "\\\"" ] ( builtins.toString value ) }\"" ;
-	    } ;
-	in
-	pkgs.stdenv.mkDerivation {
+        sh-derivation = name : sets : dependencies : pkgs.stdenv.mkDerivation {
 	    name = name ;
 	    src = ./public/scripts + ("/" + name) ;
 	    buildInputs = [ pkgs.coreutils pkgs.makeWrapper ] ;
@@ -25,22 +12,22 @@
 		    if [ -f $out/src/${ name }.sh ]
 		    then
 		        chmod 0500 $out/src/${ name }.sh &&
-			    makeWrapper $out/src/${ name }.sh $out/bin/${ name } ${ builtins.concatStringsSep " " s }
+			    makeWrapper $out/src/${ name }.sh $out/bin/${ name } ${ builtins.concatStringsSep " " ( builtins.map ( name : ( builtins.getAttr name sets ) name ) ( builtins.attrNames sets ) ) }
 		    fi
 	    '' ;
 	} ;
 	structure = structures-dir : constructor-program : options : derivations.structure structures-dir constructor-program derivations.at options ;
+	upper-case = string : builtins.replaceStrings [ "-" "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" ] [ "_" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" ] string ;
     } ;
     derivations = utils.name-it {
-        at = name : utils.sh-derivation name ( setters : [ ] ) [ pkgs.coreutils ] ;
-        destructor = name : structures-dir : utils.sh-derivation name ( setters :  [ ( setters.literal "STRUCTURES_DIR" structures-dir ) ] ) [ pkgs.coreutils pkgs.utillinux ] ;
-        foobar = name : utils.sh-derivation name ( setters : [ ( setters.literal "UUID" "59aeb05f-ae75-49de-a085-850638700e95" ) ] ) [ pkgs.coreutils ] ;
-	post-commit = name : utils.sh-derivation name ( setters : [ ( setters.literal "REMOTE" "origin" ) ] ) [ pkgs.coreutils pkgs.git ] ;
-	rebuild-nixos = name : utils.sh-derivation name ( setters : [ ( setters.literal "UUID" "59aeb05f-ae75-49de-a085-850638700e95" ) ] ) [ pkgs.coreutils pkgs.gnugrep pkgs.mktemp pkgs.rsync pkgs.systemd ] ;
-	structure = name : structures-dir : constructor-program : at : { cleaner-program ? "${ pkgs.coreutils }/bin/true" , salt-program ? "${ pkgs.coreutils }/bin/true" , seconds ? 60 } : utils.sh-derivation name ( setters : [ ( setters.literal "STRUCTURES_DIR" structures-dir ) ( setters.literal "CONSTRUCTORS_PROGRAM" constructor-program ) ( setters.literal "CLEANER_PROGRAM" cleaner-program ) ( setters.literal "SALT_PROGRAM" salt-program ) ( setters.literal "SECONDS" seconds ) ] ) [ at pkgs.coreutils pkgs.utillinux ] ;
+        at = name : utils.sh-derivation name { } [ pkgs.coreutils ] ;
+        foobar = name : uuid : utils.sh-derivation name { uuid = uuid ; } [ pkgs.coreutils ] ;
+	post-commit = name : remote : utils.sh-derivation name { remote = remote ; } [ pkgs.coreutils pkgs.git ] ;
+	rebuild-nixos = name : utils.sh-derivation name { } [ pkgs.coreutils pkgs.gnugrep pkgs.mktemp pkgs.rsync pkgs.systemd ] ;
+	structure = name : structures-dir : constructor-program : at : { cleaner-program ? "${ pkgs.coreutils }/bin/true" , salt-program ? "${ pkgs.coreutils }/bin/true" , seconds ? 60 } : utils.sh-derivation name { structures-dir = literal structures-dir ; constructor-program = literal constructor-program ; cleaner-program = literal cleaner-program ; salt-program = literal salt-program ; } [ at pkgs.coreutils pkgs.utillinux ] ;
     } ;
     structures = structures-dir : {
-        foobar = utils.structure structures-dir "${ derivations.foobar }/bin/foobar" { cleaner-program = "${ pkgs.coreutils }/bin/true" ; salt-program = "${ pkgs.coreutils }/bin/true" ; seconds = 60 * 60 ; } ;
+        foobar = uuid : utils.structure structures-dir "${ derivations.foobar uuid }/bin/foobar" { } ;
     } ;
 in {
     boot = {
@@ -89,13 +76,12 @@ in {
         extraGroups = [ "wheel" "atd" ] ;
         passwordFile = "/etc/nixos/password.asc" ;
         packages = [
-#	    pkgs.chromium
 	    pkgs.git
 	    derivations.rebuild-nixos
 	    pkgs.emacs
-	    derivations.foobar
-	    derivations.post-commit
-	    ( ( structures "/home/user/structures" ).foobar )
+	    ( derivations.foobar ( literal "b59c8073-29be-4425-966c-e215101e3448" ) )
+	    ( derivations.post-commit ( literal "origin" ) )
+	    ( ( structures "/home/user/structures" ).foobar ( literal "59dab5e4-85d1-4480-9aed-abd45142d92e" ) )
         ] ;
     } ;
 }
