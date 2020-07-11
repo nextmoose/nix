d5dc75f3d22634ba00286d971bcb78f785af3301
Emory Merryman <emory.merryman@gmail.com>
2020-07-11 21:57:36 +0000

    wtf

--- public/scripts/dot-gnupg/dot-gnupg.sh
+++ public/scripts/dot-gnupg/dot-gnupg.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-gpg --batch --import "${GPG_PRIVATE_KEYS}" &&
+export GNUPG_HOMEDIR=$(pwd) &&
+    gpg --batch --import "${GPG_PRIVATE_KEYS}" &&
     gpg --import-ownertrust "${GPG_OWNERTRUST}" &&
     gpg2 --import "${GPG2_PRIVATE_KEYS}" &&
     gpg2 --import-ownertrust "${GPG2_OWNERTRUST}" &&
