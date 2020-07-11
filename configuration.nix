e534749fdad847d3246c7ba59f7ac8d3ac2e968d
Emory Merryman <emory.merryman@gmail.com>
2020-07-11 18:50:09 +0000

    fix foobar mixup

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@
 	structure = name : structures-dir : constructor-program : at : { cleaner-program ? "${ pkgs.coreutils }/bin/true" , salt-program ? "${ pkgs.coreutils }/bin/true" , seconds ? 60 } : utils.sh-derivation name { structures-dir = literal structures-dir ; constructor-program = literal constructor-program ; cleaner-program = literal cleaner-program ; salt-program = literal salt-program ; seconds = literal seconds ; } [ at pkgs.coreutils pkgs.utillinux ] ;
     } ;
     structures = structures-dir : {
-        foo = uuid : utils.structure structures-dir "${ derivations.foobar uuid }/bin/foobar" { } ;
+        foo = uuid : utils.structure structures-dir "${ derivations.foo uuid }/bin/foo" { } ;
     } ;
 in {
     boot = {
