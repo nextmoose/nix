f4761b7e1665c77bfb8ab0111b9ba9dfcafe8e9e
Emory Merryman <emory.merryman@gmail.com>
2020-07-12 21:09:11 +0000

    wtf

--- configuration.nix
+++ configuration.nix
@@ -101,12 +101,12 @@ in {
 	    pkgs.git
 	    pkgs.gnupg
 	    pkgs.gpgme
-#	    pkgs.keychain
+	    pkgs.keychain
 #	    pkgs.pinentry
 #	    pkgs.pinentry-qt
 #	    pkgs.pinentry-emacs pkgs.pinentry-gnome pkgs.pinentry-gtk2
 	    # pkgs.pinentry_mac
-#	    pkgs.signing-party
+ 	    pkgs.signing-party
 	    pkgs.pinentry-curses
 	    derivations.rebuild-nixos
 	    pkgs.emacs
