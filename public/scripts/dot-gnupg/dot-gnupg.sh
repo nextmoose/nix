23f002f0298cb59c3d23a88a05104bafce9c18c0
Emory Merryman <emory.merryman@gmail.com>
2020-07-12 19:22:07 +0000

    no batch on input

--- public/scripts/dot-gnupg/dot-gnupg.sh
+++ public/scripts/dot-gnupg/dot-gnupg.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 export GNUPGHOME=$(pwd) &&
+    chmod 0700 "${GNUPGHOME}" &&
     gpg --import "${GPG_PRIVATE_KEYS}" &&
     gpg --import-ownertrust "${GPG_OWNERTRUST}" &&
     gpg2 --import "${GPG2_PRIVATE_KEYS}" &&
     gpg2 --import-ownertrust "${GPG2_OWNERTRUST}" &&
-    chmod 0700 "${GNUPGHOME}" &&
     true
