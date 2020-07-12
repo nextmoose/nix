35c0d3a8ff0d5a4d48f4532ceee84f05a48c8bd5
Emory Merryman <emory.merryman@gmail.com>
2020-07-12 01:49:18 +0000

    fix perms

--- public/scripts/dot-gnupg/dot-gnupg.sh
+++ public/scripts/dot-gnupg/dot-gnupg.sh
@@ -5,4 +5,5 @@ export GNUPGHOME=$(pwd) &&
     gpg --import-ownertrust "${GPG_OWNERTRUST}" &&
     gpg2 --import "${GPG2_PRIVATE_KEYS}" &&
     gpg2 --import-ownertrust "${GPG2_OWNERTRUST}" &&
+    chmod 0700 "${GNUPGHOME}" &&
     true
