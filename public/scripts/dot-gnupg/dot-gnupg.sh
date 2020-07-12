77cb0c234c7499a094f39cb221e9bda9e4e6746c
Emory Merryman <emory.merryman@gmail.com>
2020-07-12 19:16:26 +0000

    no batch on input

--- public/scripts/dot-gnupg/dot-gnupg.sh
+++ public/scripts/dot-gnupg/dot-gnupg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export GNUPGHOME=$(pwd) &&
-    gpg --batch --import "${GPG_PRIVATE_KEYS}" &&
+    gpg --import "${GPG_PRIVATE_KEYS}" &&
     gpg --import-ownertrust "${GPG_OWNERTRUST}" &&
     gpg2 --import "${GPG2_PRIVATE_KEYS}" &&
     gpg2 --import-ownertrust "${GPG2_OWNERTRUST}" &&
