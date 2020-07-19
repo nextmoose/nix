cca446c4c276615e70977bf645967854e2d03b25
Emory Merryman <emory.merryman@gmail.com>
2020-07-19 11:46:28 +0000

    wtf

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,7 @@
 	        makeWrapper ${ pkgs.pass }/bin/pass $out/bin/${ executable-name } ${ dot-gnupg "dot-gnupg" } ${ password-store-dir "password-store-dir" } --run "export PASSWORD_STORE_GPG_OPTS=\"--homedir \$DOT_GNUPG\"" --run "export GPG_TTY=\$(${pkgs.coreutils}/bin/tty)" --run "export PASSWORD_STORE_ENABLE_EXTENSIONS=true" --run "export PASSWORD_STORE_EXTENSIONS_DIR=\"$out/extensions\"" --set PATH ${ pkgs.lib.makeBinPath [ pkgs.pinentry pkgs.pinentry-qt ] }
 		${ builtins.concatStringsSep " && " ( builtins.map ( name : "makeWrapper ${ ( builtins.getAttr name extensions ).program } $out/extensions/${ name }.bash" ) ( builtins.attrNames extensions ) ) }
 		mkdir $out/completions
+		prefix=$( echo ${ password-store-dir "password-store-dir" } | cut --fields 3 --delimiter " " )
 		echo sed \
 		    -e "s#_pass#_pass_$( basename $out )_#" \
 		    -e "s# pass# ${ executable-name }#" \
