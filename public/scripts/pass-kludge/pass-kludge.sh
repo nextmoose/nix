57b7236c338bcda79c935561e0405d7441cd9444
Emory Merryman <emory.merryman@gmail.com>
2020-07-19 00:04:18 +0000

    this kludge works good enough

--- public/scripts/pass-kludge/pass-kludge.sh
+++ public/scripts/pass-kludge/pass-kludge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-read -s -p "PASSPHRASE" PASSPHRASE &&
+read -s -p "PASSPHRASE:  " PASSPHRASE &&
     export GPGHOME=$( echo ${PASSWORD_STORE_GPG_OPTS} | cut --fields 2 --delimiter " " ) &&
     echo "${PASSPHRASE}" | gpg --homedir "${GPGHOME}" --batch --pinentry-mode loopback --passphrase-fd 0 --decrypt "${PASSWORD_STORE_DIR}/${@}.gpg" &&
     true
