1908fcaf58d5fccff39092550dca3e6018729c3d
Emory Merryman <emory.merryman@gmail.com>
2020-07-19 17:09:48 +0000

    fix personal-identification-number script

--- public/scripts/personal-identification-number/personal-identification-number.sh
+++ public/scripts/personal-identification-number/personal-identification-number.sh
@@ -5,7 +5,7 @@ then
     touch "${FILE_NAME}" &&
 	true
 else
-    cat /dev/urandom | tr --delete --complement "0-9" | fold --width 32 | head > "${FILE_NAME}" &&
+    cat /dev/urandom | tr --delete --complement "0-9" | fold --width 32 | head --lines 1 > "${FILE_NAME}" &&
 	true
 fi &&
     chmod 0400 "${FILE_NAME}" &&
