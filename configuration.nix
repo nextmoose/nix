5f47639637a54cd9f6b16047112a85108fba4151
Emory Merryman <emory.merryman@gmail.com>
2020-07-21 11:07:32 +0000

    adding a replace-strings utility method

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,8 @@
 { config, pkgs, ... } : let
     literal = value : name : "--run \"export ${ utils.upper-case name }=${ utils.replace-strings ( builtins.toString value ) }\"" ;
-    literals = values : "--add-flags ${ builtins.concatStringsSep " " ( builtins.map ( value : utils.replace-strings ( builtins.toString value ) ) values ) }" ;
+    literals = values : name : "--add-flags ${ builtins.concatStringsSep " " ( builtins.map ( value : utils.replace-strings ( builtins.toString value ) ) values ) }" ;
     structure-dir = value : name : "--run \"export ${ utils.upper-case name }=${ utils.replace-strings ( builtins.concatStringsSep "" [ "\$( " ( builtins.toString value ) "/bin/structure )" ] ) }\"" ;
-    structure-dirs = values : name : "--add-flags ${ builtins.concatStringsSep " " ( builtins.map ( value : "typeof=${ builtins.typeOf value }" ) values ) }" ;
-    tstructure-dirs = values : name : "--add-flags ${ builtins.concatStringsSep " " ( builtins.map ( value : utils.replace-strings ( "$( ${ value }/bin/structure )" ) ) values ) }" ;
+    structure-dirs = values : name : "--add-flags FLAGS" ;
     structure-file = value : file-name : name : "--run \"export ${ utils.upper-case name }=${ utils.replace-strings ( builtins.concatStringsSep "" [ "\$( " ( builtins.toString value ) "/bin/structure )" "/" file-name ] ) }\"" ;
     structure-cat = value : file-name : name : "--run \"export ${ utils.upper-case name }=${ utils.replace-strings ( builtins.concatStringsSep "" [ "\$( " pkgs.coreutils "/bin/cat " "\$( " ( builtins.toString value ) "/bin/structure )" "/" file-name " )" ] ) }\"" ;
     utils = {
