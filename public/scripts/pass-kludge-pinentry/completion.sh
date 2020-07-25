0e6966d0e6d6cce28ca6fa49f922ee0737970c88
Emory Merryman <emory.merryman@gmail.com>
2020-07-25 21:27:50 +0000

    try 3rd phase

--- public/scripts/pass-kludge-pinentry/completion.sh
+++ public/scripts/pass-kludge-pinentry/completion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # To add completion for an extension command define a function like this:
-__pass_dslwwnw3v74dl39d5sz2zy488qbs6p57-password_store_extension_complete_<COMMAND>() {
+__pass_OUT-password_store_extension_complete_COMMAND() {
     COMPREPLY+=($(compgen -W "-o --option" -- ${cur}))
-    _pass_dslwwnw3v74dl39d5sz2zy488qbs6p57-pass_complete_entries 1
+    _pass_OUT-pass_complete_entries 1
 }
