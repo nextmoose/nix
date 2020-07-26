82489193443ea42fc901d687f74bf324c5ef5129
Emory Merryman <emory.merryman@gmail.com>
2020-07-26 18:34:48 +0000

    do it

--- default.nix
+++ default.nix
@@ -65,7 +65,15 @@
 	        mkdir $out &&
 		    cp --recursive . $out/src &&
 		    chmod 0500 $out/src/multiple-site-dot-ssh.sh &&
-		    makeWrapper $out/src/multiple-site-dot-ssh.sh $out/bin/multiple-site-dot-ssh ${ configs.format builtins.toString }
+		    echo &&
+		    echo &&
+		    echo &&
+		    echo ${ configs.format builtins.typeOf } &&
+		    echo &&
+		    echo &&
+		    echo &&
+		    echo &&
+		    makeWrapper $out/src/multiple-site-dot-ssh.sh $out/bin/multiple-site-dot-ssh
 	    '' ;
 	} ;
 	pass = name : program-name : dot-gnupg : password-store-dir : extensions : pkgs.stdenv.mkDerivation {
