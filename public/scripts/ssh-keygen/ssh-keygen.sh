8264fc8ae1785bd90f49ebe88bfd26375db27fab
Emory Merryman <emory.merryman@gmail.com>
2020-07-26 21:57:09 +0000

    fix pin

--- public/scripts/ssh-keygen/ssh-keygen.sh
+++ public/scripts/ssh-keygen/ssh-keygen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo ssh-keygen -f ./id-rsa -P "${PASSPHRASE}" -C "" &&
-    ssh-keygen -f ./id-rsa -P "${PASSPHRASE}" -C "" &&
-    chmod 0400 .id-rsa .id-rsa.pub &&
+echo ssh-keygen -f id-rsa -P "${PASSPHRASE}" -C "" &&
+    ssh-keygen -f id-rsa -P "${PASSPHRASE}" -C "" &&
+    chmod 0400 id-rsa id-rsa.pub &&
     true
