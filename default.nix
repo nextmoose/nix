007c34691ccbd28553af5702b47ccc2a4b1abbb7
Emory Merryman <emory.merryman@gmail.com>
2020-07-26 20:52:48 +0000

    do it

--- default.nix
+++ default.nix
@@ -166,7 +166,7 @@ in {
         } ;
     } ;
     shell = let
-        dot-ssh = structures.multiple-site-dot-ssh [ ( structure-dir upstream-dot-ssh ) ( structure-dir personal-dot-ssh ) ( structure-dir report-dot-ssh ) ] ;
+        dot-ssh = structures.multiple-site-dot-ssh [ ( structure-file ( literal "config" ) upstream-dot-ssh ) ( structure-file ( literal "config" ) personal-dot-ssh ) ( structure-file ( literal "config" ) report-dot-ssh ) ] ;
 	personal-dot-ssh = structures.single-site-dot-ssh ( literal "personal" ) ( literal "github.com" ) ( literal "git" ) ( literal 22 ) ( structure-file "id-rsa" personal-id-rsa ) ( structure-file "secret.asc" user-known-hosts-file ) ;
         personal-id-rsa = structures.ssh-keygen ( structure-dir ( structures.personal-identification-number ( literal 0 ) ( literal "a6104037-4036-4cde-8b10-a8de9f6e3145" ) ) ) ;
 	report-dot-ssh = structures.single-site-dot-ssh ( literal "report" ) ( literal "github.com" ) ( literal "git" ) ( literal 22 ) ( structure-file "id-rsa" report-id-rsa ) ( structure-file "secret.asc" user-known-hosts-file ) ;
