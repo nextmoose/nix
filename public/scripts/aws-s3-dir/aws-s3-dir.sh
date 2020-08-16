77342a7e7e734177c4c95b93e161f712475a813d
Emory Merryman <emory.merryman@gmail.com>
2020-08-16 15:29:40 +0000

    one step backwards

--- public/scripts/aws-s3-dir/aws-s3-dir.sh
+++ public/scripts/aws-s3-dir/aws-s3-dir.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-if [ -z "$( aws s3 ls "s3://${BUCKET}" )" ]
+if [ -z "$( aws s3api list-buckets --output text --query "Buckets[?Name=='${BUCKET}'].Name" )" ]
 then
-    aws s3 cp "s3://${BUCKET}" . --recursive &&
+    aws s3api create-bucket --acl private --bucket "${BUCKET}" &&
+	true
+else
+    aws s3 cp "s3://${BUCKET}" . --recursive --sse &&
 	true
 fi &&
     true
